e03a00a771a3002fc47730172dda36825d656104
Joviel Her <37461428+Phant0Meow@users.noreply.github.com>
2026-06-05 14:40:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="fem",
     version="1.1.0",
-    py_modules=["main"],   # 明确把 main.py 作为模块
+    py_modules=["femCompiler"], 
     entry_points={
         "console_scripts": [
-            "fem = main:main",
+            "fem = femCompiler:main",
         ],
     },
 )
